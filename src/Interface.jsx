83a2020e22a9fa5641f42c4a89313d4af804c28a
Daniel Slattery <daniel.slattery1@gmail.com>
2024-05-22 15:23:33 +0100

    code formatting

--- src/Interface.jsx
+++ src/Interface.jsx
@@ -1,27 +1,27 @@
-import { useKeyboardControls } from '@react-three/drei'
+import { useKeyboardControls } from '@react-three/drei';
 
-export default function Interface()
-{
-    const forward = useKeyboardControls((state) => state.forward)
-    const backward = useKeyboardControls((state) => state.backward)
-    const leftward = useKeyboardControls((state) => state.leftward)
-    const rightward = useKeyboardControls((state) => state.rightward)
-    const jump = useKeyboardControls((state) => state.jump)
+export default function Interface() {
+  const forward = useKeyboardControls((state) => state.forward);
+  const backward = useKeyboardControls((state) => state.backward);
+  const leftward = useKeyboardControls((state) => state.leftward);
+  const rightward = useKeyboardControls((state) => state.rightward);
+  const jump = useKeyboardControls((state) => state.jump);
 
-    return <div className="interface">
-        <div className="controls">
-            <div className="raw">
-                <div className={ `key ${ forward ? 'active' : '' }` }></div>
-            </div>
-            <div className="raw">
-                <div className={ `key ${ leftward ? 'active' : '' }` }></div>
-                <div className={ `key ${ backward ? 'active' : '' }` }></div>
-                <div className={ `key ${ rightward ? 'active' : '' }` }></div>
-            </div>
-            <div className="raw">
-                <div className={ `key large ${ jump ? 'active' : '' }` }></div>
-            </div>
+  return (
+    <div className="interface">
+      <div className="controls">
+        <div className="raw">
+          <div className={`key ${forward ? 'active' : ''}`}></div>
         </div>
-
+        <div className="raw">
+          <div className={`key ${leftward ? 'active' : ''}`}></div>
+          <div className={`key ${backward ? 'active' : ''}`}></div>
+          <div className={`key ${rightward ? 'active' : ''}`}></div>
+        </div>
+        <div className="raw">
+          <div className={`key large ${jump ? 'active' : ''}`}></div>
+        </div>
+      </div>
     </div>
+  );
 }
--- src/Level.jsx
+++ src/Level.jsx
@@ -8,41 +8,37 @@ const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' });
 
 export function Ground({ position = [0, 0, 0], scale = 20 }) {
-    return (
-      <group position={position}>
-        <Float floatIntensity={0.25} rotationIntensity={0.25}>
-          <Text
-            font="/bebas-neue-v9-latin-regular.woff"
-            scale={0.5}
-            maxWidth={0.25}
-            lineHeight={0.75}
-            textAlign="left"
-            position={[0.75, 0.65, 0]}
-            rotation-y={-0.25}
-          >
-            Pixel Playground
-            <meshBasicMaterial toneMapped={false} />
-          </Text>
-        </Float>
-        <mesh
-          geometry={boxGeometry}
-          material={floor1Material}
-          position={[0, -0.1, 0]}
-          scale={[scale, 0.2, scale]}
-          receiveShadow
-        />
-        <CuboidCollider
-          args={[scale/2, 0.1, scale/2]}
-          position={[0, -0.1, 0]}
-        />
-      </group>
-    );
-  }
+  return (
+    <group position={position}>
+      <Float floatIntensity={0.25} rotationIntensity={0.25}>
+        <Text
+          font="/bebas-neue-v9-latin-regular.woff"
+          scale={0.5}
+          maxWidth={0.25}
+          lineHeight={0.75}
+          textAlign="left"
+          position={[0.75, 0.65, 0]}
+          rotation-y={-0.25}
+        >
+          Pixel Playground
+          <meshBasicMaterial toneMapped={false} />
+        </Text>
+      </Float>
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        scale={[scale, 0.2, scale]}
+        receiveShadow
+      />
+      <CuboidCollider
+        args={[scale / 2, 0.1, scale / 2]}
+        position={[0, -0.1, 0]}
+      />
+    </group>
+  );
+}
 
 export function Level() {
-  return (
-    <>
-      <Ground />
-    </>
-  );
+  return <Ground />;
 }
