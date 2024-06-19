2243cde5f6f6189fce6559d492aabe4875a9e62b
Daniel Slattery <daniel.slattery1@gmail.com>
2024-06-19 18:01:49 +0100

    updated origin allowed dependant on environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,50 +1,54 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+import {createServer} from 'http'
+import {Server} from 'socket.io'
+import dotenv from 'dotenv'
 
-const httpServer = createServer();
+dotenv.config()
 
-const io = new Server(httpServer);
+const httpServer = createServer()
 
-// List of allowed origins
-const allowedOrigins = [
-  'http://pixelplayground.s3-website.eu-west-2.amazonaws.com',
-  'http://localhost:5173'
-];
+const io = new Server(httpServer)
+
+// Dynamic list of allowed origins based on environment variables
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',') || []
 
 // Disallow connections from outside the specified origins
 io.use((socket, next) => {
-  const origin = socket.handshake.headers.origin;
+  const origin = socket.handshake.headers.origin
   if (origin && !allowedOrigins.includes(origin)) {
-    return next(new Error('origin not allowed'));
+    return next(new Error('origin not allowed'))
   }
-  next();
-});
+  next()
+})
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
-const avatars: { [id: string]: { position: { x: number; y: number; z: number } } } = {};
+const avatars: {[id: string]: {position: {x: number; y: number; z: number}}} =
+  {}
 
-io.on('connection', (socket) => {
-  console.log('New client connected:', socket.id);
+io.on('connection', socket => {
+  console.log('New client connected:', socket.id)
 
   // Send the initial state of all avatars to the new client
-  socket.emit('initial-avatars', avatars);
+  socket.emit('initial-avatars', avatars)
 
   // Handle new position updates
-  socket.on('move', (data) => {
-    avatars[socket.id] = data.position;
+  socket.on('move', data => {
+    avatars[socket.id] = data.position
     // Broadcast the movement to other users
-    socket.broadcast.emit('avatar-moved', { id: socket.id, position: data.position });
-  });
+    socket.broadcast.emit('avatar-moved', {
+      id: socket.id,
+      position: data.position
+    })
+  })
 
   // Handle user disconnection
   socket.on('disconnect', () => {
-    console.log('Client disconnected:', socket.id);
-    delete avatars[socket.id];
-    io.emit('avatar-disconnected', socket.id);
-  });
-});
+    console.log('Client disconnected:', socket.id)
+    delete avatars[socket.id]
+    io.emit('avatar-disconnected', socket.id)
+  })
+})
 
 httpServer.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
